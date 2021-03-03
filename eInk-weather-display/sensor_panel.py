9591a50967a63d9a8ecabd29cc9c9fc16859b520
Severi Salminen <severi.salminen@futurice.com>
2021-03-03 22:53:02 +0200

    Fix typos and references

--- eInk-weather-display/sensor_panel.py
+++ eInk-weather-display/sensor_panel.py
@@ -14,11 +14,11 @@ def get_sensor_panel(fonts, config):
   draw = ImageDraw.Draw(image)
 
   logger.info('Fetching sensor data')
-  sensor_data = RuuviTagSensor.get_data_for_sensors([config.get('MAC_IN')], 5)
+  sensor_data = RuuviTagSensor.get_data_for_sensors([config.get('RUUVITAG_MAC_IN')], 5)
   logger.info(sensor_data)
 
-  if (config.get('MAC_IN') in sensor_data):
-    state_in = sensor_data[config.get('MAC_IN')]
+  if (config.get('RUUVITAG_MAC_IN') in sensor_data):
+    state_in = sensor_data[config.get('RUUVITAG_MAC_IN')]
     draw.text((x_size/2, 0), 'In', font = fonts['font_sm'], fill = 0, anchor = 'ma')
     utils.draw_quantity(draw, (x_size/2, 35), str(round(state_in['temperature'], 1)), '°C', fonts, 'font_sm')
     utils.draw_quantity(draw, (x_size/2, 55), str(round(state_in['humidity'], 1)), '%', fonts, 'font_sm')
