abe8939f9055de81f1275e8e9596b758fdc7d7e9
Severi Salminen <severi.salminen@futurice.com>
2021-03-10 23:23:51 +0200

    Update the script

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -3,7 +3,6 @@
 # Converts all SVG icons to PNG images using rsvg-convert. 
 # You might need to install it before using the script.
 
-rm png_icons/*
 rm eInk-weather-display/png_icons/*
 for f in svg_icons/*.svg
 do
