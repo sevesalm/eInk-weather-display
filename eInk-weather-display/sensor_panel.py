71cd70fdae8ab77c5b0de84dcffd2cf1fbcb4d45
Severi Salminen <severi.salminen@futurice.com>
2021-04-03 10:23:44 +0300

    Show RuuviTag sensor battery voltage

--- eInk-weather-display/sensor_panel.py
+++ eInk-weather-display/sensor_panel.py
@@ -5,11 +5,11 @@ import utils
 def get_battery_icon(voltage, images):
   if (voltage >= 2900):
     return images['misc']['battery_full']
-  if (voltage >= 2700):
+  if (voltage >= 2800):
     return images['misc']['battery_75']
-  if (voltage >= 2500):
+  if (voltage >= 2600):
     return images['misc']['battery_50']
-  if (voltage >= 2300):
+  if (voltage >= 2400):
     return images['misc']['battery_25']
   return images['misc']['battery_empty']
 
@@ -25,17 +25,18 @@ def get_sensor_panel(images, fonts, config):
   
   utils.draw_title(draw, (120, 80), 'IN', fonts)
 
-  logger.info('Fetching sensor data')
   sensor_mac = config.get('RUUVITAG_MAC_IN')
   try:
     if (not config.getboolean('FILE_OUTPUT')):
+      timeout = config.getint('SENSOR_POLL_TIMEOUT')
+      logger.info(f'Fetching sensor data (timeout: {timeout})')
       from ruuvitag_sensor.ruuvi_rx import RuuviTagReactive
       import rx
       ruuvi_reactive = RuuviTagReactive([sensor_mac])
       sensor_data = ruuvi_reactive\
         .get_subject()\
         .map(lambda x: {x[0]: x[1]})\
-        .merge(rx.Observable.timer(config.getint('SENSOR_POLL_TIMEOUT')).map(lambda x: {}))\
+        .merge(rx.Observable.timer(timeout).map(lambda x: {}))\
         .to_blocking()\
         .first()
       ruuvi_reactive.stop() 
@@ -51,9 +52,10 @@ def get_sensor_panel(images, fonts, config):
     data_y_base = 150
     state_in = sensor_data[sensor_mac]
     utils.draw_quantity(draw, (x_size//2 + 150, data_y_base), str(round(state_in['temperature'], 1)), '°C', fonts, 'font_lg', 'font_sm')
-    utils.draw_quantity(draw, (x_size//2 + 150, data_y_base + 90), str(round(state_in['humidity'])), '%', fonts, 'font_sm')
+    utils.draw_quantity(draw, (x_size//2 + 150, data_y_base + 90), str(round(state_in['humidity'])), '%', fonts)
     battery_icon = get_battery_icon(state_in['battery'], images)
     image.paste(battery_icon, (10, 80), ImageOps.invert(battery_icon))
+    utils.draw_quantity(draw, (130, 245), str(round(state_in['battery']/1000, 2)), 'V', fonts, 'font_xs', 'font_xxs')
   else: 
     logger.info(f'Could not find mac {sensor_mac} in sensor data')
     no_wifi_image = images['misc']['no_wifi']
