2b5a631d3609b903d55a7d13ca25afd97be1d792
Severi Salminen <severi.salminen@futurice.com>
2021-04-03 10:28:14 +0300

    Increase icon image size

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -7,5 +7,5 @@ rm eInk-weather-display/png_icons/*
 for f in svg_icons/*.svg
 do
   echo Converting $f
-  rsvg-convert -w 200 $f > eInk-weather-display/png_icons/$(basename $f .svg).png
+  rsvg-convert -w 400 $f > eInk-weather-display/png_icons/$(basename $f .svg).png
 done
