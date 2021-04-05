cfd691cc153a001cb4b529a65f4f23a54b216bb3
Severi Salminen <severi.salminen@futurice.com>
2021-04-05 18:07:25 +0300

    Improve alignment

--- eInk-weather-display/info_panel.py
+++ eInk-weather-display/info_panel.py
@@ -5,13 +5,13 @@ import logging
 def get_info_panel(fonts, config):
   logger = logging.getLogger(__name__)
   logger.info('Generating info panel')
-  x_size = 270
+  x_size = 260
   y_size = 200
   image = Image.new('L', (x_size, y_size), 0xff)
   draw = ImageDraw.Draw(image)
   
-  draw.text((x_size - 20, 20), f'{time.strftime("%-H:%M")}', fill="black", font=fonts['font_md'], anchor='rt')
-  draw.text((x_size - 20, y_size - 20), f'{time.strftime("%-d.%-m.%Y")}', fill="black", font=fonts['font_sm'], anchor='rs')
+  draw.text((x_size//2, 20), f'{time.strftime("%-H:%M")}', fill="black", font=fonts['font_md'], anchor='mt')
+  draw.text((x_size//2, y_size - 20), f'{time.strftime("%-d.%-m.%Y")}', fill="black", font=fonts['font_sm'], anchor='ms')
 
   # Borders
   if (config.getboolean('DRAW_PANEL_BORDERS')):
