c1c4a50618022dd1a4740e4374648f949c78c04b
Severi Salminen <severi.salminen@futurice.com>
2022-04-23 14:03:17 +0300

    Handle possible random freezes using a timeout

--- eInk-weather-display/refresh.py
+++ eInk-weather-display/refresh.py
@@ -10,8 +10,10 @@ from timeit import default_timer as timer
 from configparser import SectionProxy
 from typing import Optional
 from type_alias import Icons, Fonts
+from multiprocessing import Process
 
-def refresh(panel_size: tuple[int, int], fonts: Fonts, images: DusksAndDawns, config: SectionProxy, epd_so: Optional[ctypes.CDLL], init: bool, error_count: int) -> None:
+PROCESS_TIMEOPUT = 10 # In seconds
+
   logger = logging.getLogger(__name__)
   if (init == True):
     logger.info('Full refresh started')
@@ -57,11 +59,22 @@ def refresh(panel_size: tuple[int, int], fonts: Fonts, images: DusksAndDawns, co
       full_image = full_image.transpose(Image.FLIP_LEFT_RIGHT)
     image_bytes = full_image.rotate(0 if not config.getboolean('ROTATE_180') else 180, expand=True).tobytes()
     c_logger = ctypes.CFUNCTYPE(None, ctypes.c_int, ctypes.c_wchar_p)(logging.getLogger('esp.so').log)
-    start_refresh_time = timer()
-    result = epd_so.draw_image_8bit(image_bytes, ctypes.c_bool(init), ctypes.c_int(config.getint('EPD_VOLTAGE')), config.getint('BITS_PER_PIXEL'), c_logger)
-    elapsed_refresh_time = timer() - start_refresh_time
-    if(result != 0):
-      logger.error('There was an error when calling draw_image_8bit()')
+    if(epd_so):
+      start_refresh_time = timer()
+      try:
+        p = Process(target=epd_so.draw_image_8bit, args=(image_bytes, ctypes.c_bool(init), ctypes.c_int(config.getint('EPD_VOLTAGE')), config.getint('BITS_PER_PIXEL'), c_logger))
+        p.start()
+        p.join(PROCESS_TIMEOPUT)
+        logger.debug(f'Exit code: {p.exitcode}')
+        if (p.exitcode == None):
+          logger.error('An error occured during draw_image_8bit()')
+        p.terminate()
+      except Exception as e:
+        logger.exception('Unexpected error: %s', str(e))
+      finally:
+        elapsed_refresh_time = timer() - start_refresh_time
+    else:
+      raise Exception('epd_so not defined')
 
   elapsed_time = timer() - start_time
   logger.info(f'Total time: {round(elapsed_time, 1)} s, refresh: {round(elapsed_refresh_time, 1)} s')
