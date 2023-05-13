4df49d0011793497a8c4a6e9672ed5c60bd6ad2f
Severi Salminen <severi.salminen@futurice.com>
2023-05-13 12:22:43 +0300

    Show week number

--- eInk-weather-display/info_panel.py
+++ eInk-weather-display/info_panel.py
@@ -15,12 +15,13 @@ def get_info_panel(fonts: Fonts, config: SectionProxy) -> Image.Image:
   draw = ImageDraw.Draw(image)
   utils.draw_title(draw, fonts['font_sm'], 'INFO', None, fonts['font_xxs'])
 
-  draw.text((x_size//2, 150), f'{time.strftime("%a %-d.%-m.")}', fill="black", font=fonts['font_sm'], anchor='ms')
-  draw.text((x_size//2, 170), f'{time.strftime("%-H:%M")}', fill="black", font=fonts['font_md'], anchor='mt')
-  draw.text((x_size//2, 320), f'{time.strftime("%Y")}', fill="black", font=fonts['font_xs'], anchor='ms')
+  draw.text((x_size//2, 90), f'{time.strftime("%-H:%M")}', fill="black", font=fonts['font_md'], anchor='mt')
+  draw.text((x_size//2, 250), f'{time.strftime("%a %-d.%-m.")}', fill="black", font=fonts['font_sm'], anchor='ms')
+  draw.text((x_size//2, 300), f'{time.strftime("Week %W")}', fill="black", font=fonts['font_xs'], anchor='ms')
+  draw.text((x_size//2, 350), f'{time.strftime("%Y")}', fill="black", font=fonts['font_xs'], anchor='ms')
 
   if (config.getboolean('DEV_MODE')):
-    draw.text((x_size//2, 370), '(DEV MODE)', fill="black", font=fonts['font_xxs'], anchor='ms')
+    draw.text((x_size//2, 400), '(DEV MODE)', fill="black", font=fonts['font_xxs'], anchor='ms')
 
   # Borders
   if (config.getboolean('DRAW_PANEL_BORDERS')):
