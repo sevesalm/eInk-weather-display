55e94e08b05811014378c3c9aed9176afccea1b9
Severi Salminen <severi.salminen@futurice.com>
2023-12-06 13:55:57 +0200

    Split requirements to different environments

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 source scripts/config.env
 
 ssh $HOSTNAME "mkdir -p $VENV_ROOT_DIRECTORY"
-rsync -av --delete requirements.txt $HOSTNAME:$VENV_ROOT_DIRECTORY
+rsync -av --delete requirements $HOSTNAME:$VENV_ROOT_DIRECTORY
 ssh $HOSTNAME "cd $VENV_ROOT_DIRECTORY && python -m venv $VENV_NAME"
-ssh $HOSTNAME "source $VENV_DIRECTORY/bin/activate && pip install -r $VENV_ROOT_DIRECTORY/requirements.txt && rm $VENV_ROOT_DIRECTORY/requirements.txt"
+ssh $HOSTNAME "source $VENV_DIRECTORY/bin/activate && pip install -r $VENV_ROOT_DIRECTORY/requirements/raspberry.txt && rm -rf $VENV_ROOT_DIRECTORY/requirements"
