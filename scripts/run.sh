1c8d928399e0b62c57e16e3368a9d77779a79c90
Severi Salminen <severi.salminen@futurice.com>
2024-02-07 21:37:33 +0200

    Fix typo

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 source ./config.env
 
 . ${VENV_DIRECTORY}/bin/activate
-sudo RUUVI_BLE_ADAPTER="Bleson" ${VENV_DIRECTORY}/bin/python ${APP_DIRECTORY}/weather-display.py; exec ${SHELL}  # Leaves the tmux session running so we can see errors
+sudo RUUVI_BLE_ADAPTER="bleson" ${VENV_DIRECTORY}/bin/python ${APP_DIRECTORY}/weather-display.py; exec ${SHELL}  # Leaves the tmux session running so we can see errors
